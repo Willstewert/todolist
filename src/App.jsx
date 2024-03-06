19ac8ad36b09c4b16775d3c5b41b957081286c01
Willstewert <146980562+Willstewert@users.noreply.github.com>
2024-03-05 21:03:41 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,17 +29,18 @@ function App() {
 
   return (
     <>
-    <div className="w-[300px]">
-    <div className="flex items-center justify-between">
+    <div className=  "flex items-center justify-center bg-slate-800 w-[100%] h-[100vh]">
+    <div className="w-[400px] bg-slate-700 p-5 rounded-md">
+    <div className="flex items-center justify-between ">
     <input
-        className="border rounded-md border-slate-800 outline-none"
+        className="border rounded-md border-slate-800 outline-none p-1"
         type="text"
         onChange={(e) => setContent(e.target.value)}
         name=""
         id=""
       />
       <button
-        className="border border-black rounded-sm px-5 hover:bg-slate-700 hover:text-white"
+        className="bg-slate-900 text-white rounded-md px-5 hover:bg-slate-500 hover:text-white p-1"
         onClick={() => dispatch(add(content))}
       >
         Add
@@ -47,18 +48,18 @@ function App() {
     </div>
       {listItems.map((item) => (
         <div key={item.id}>
-          <div className="flex items-center justify-between ">
+          <div className="flex items-center justify-between  ">
             {item.edit ? (
               <p>{item.data}</p>
             ) : (
               <input
-                className="border border-black outline-none"
+                className="border rounded-md border-slate-800 outline-none p-1 bg-slate-600"
                 type="text"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
               />
             )}
-            <div className="flex items-center">
+            <div className="flex items-center mt-2 p-1 ">
             {item.edit ? (
               <button
                 className="border rounded-sm border-black p-1 hover:bg-slate-700 hover:text-white"
@@ -88,6 +89,7 @@ function App() {
         </div>
         
       ))}
+      </div>
       </div>
     </>
   );
